fc39ded7f032f5e01603f30e98d14b938dcab46f
ZheerBarzan <zheerbarzan85@outlook.com>
2025-01-30 19:37:43 +0300

    Initial Commit

---
+++ Medusa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Medusa
+//
+//  Created by zheer barzan on 30/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Medusa/MedusaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedusaApp.swift
+//  Medusa
+//
+//  Created by zheer barzan on 30/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MedusaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
