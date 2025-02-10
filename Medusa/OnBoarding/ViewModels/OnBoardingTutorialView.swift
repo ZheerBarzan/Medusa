d47301147f313a404f3306e31ea7f3ca05aaf9ae
ZheerBarzan <zheerbarzan85@outlook.com>
2025-02-10 17:34:29 +0300

    finished with the OnBoarding and added the tutorial videos which will guide the user to caputer the object or scene

--- Medusa/OnBoarding/ViewModels/OnBoardingTutorialView.swift
+++ Medusa/OnBoarding/ViewModels/OnBoardingTutorialView.swift
@@ -16,7 +16,86 @@ struct OnBoardingTutorialView: View {
     var viewSize: CGSize
     
     var body: some View {
-        Text("OnBoarding Tutorial View")
+        VStack{
+            let frameSize = min(viewSize.width, viewSize.height) * (UIDevice.current.userInterfaceIdiom == .pad ? 0.6 : 0.8)
+            switch appModel.captureMode {
+            case .object:
+                ZStack{
+                    if showTutorialInReview, let url = tutorialURL{
+                        TutorialVideoView(url: url, inReview: true)
+                    }else{
+                        VStack{
+                            Spacer()
+                            ObjectCapturePointCloudView(session: session)
+                                .showShotLocations()
+                            Spacer()
+                        }
+                    }
+                    
+                    VStack{
+                        Spacer()
+                        HStack{
+                            ForEach(AppDataModel.Orbit.allCases){ orbit in
+                                if let orbitImageName = getOrbitImageName(orbit: orbit){
+                                    Text(Image(systemName: orbitImageName))
+                                        .font(.system(size: 30))
+                                        .foregroundColor(.gray)
+                                    
+                                }
+                            }
+                            
+                        }
+                    }
+                    
+                    
+                }.frame(width: frameSize, height: frameSize)
+                
+            case .scene:
+                Spacer().frame(height: 50)
+                ObjectCapturePointCloudView(session: session)
+                    .showShotLocations( shotLocations)
+                    .frame(width: frameSize, height: frameSize)
+            }
+            VStack{
+                Text(title)
+                    .font(.title)
+                    .lineLimit(3)
+                    .multilineTextAlignment(.center)
+                    .minimumScaleFactor(0.4)
+                    .bold()
+                    .padding(.bottom)
+                    .frame(maxWidth: .infinity)
+                
+                Text(detail)
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: .infinity)
+                
+                Spacer()
+                
+                if appModel.captureMode == .scene{
+                    Text(LocalizedString.estimatedProcessingTime)
+                        .font(.caption2)
+                        .foregroundColor(.secondary)
+                    
+                    if let numberOfShotsTaken = appModel.objectCaptureSession?.numberOfShotsTaken,
+                       let maximumNumberOfShots = appModel.objectCaptureSession?.maximumNumberOfInputImages,
+                       numberOfShotsTaken > maximumNumberOfShots{
+                        Text(String(format: LocalizedString.transferToMacGuidance,
+                                    maximumNumberOfShots, numberOfShotsTaken))
+                        .font(.caption2)
+                        .foregroundStyle(.secondary)
+                        .multilineTextAlignment(.center)
+                    }
+                    Spacer().frame(height: 130)
+                    
+                }
+                
+            }
+            .foregroundColor(.primary)
+            .frame(maxHeight: .infinity)
+            .padding([.leading, .trailing], UIDevice.current.userInterfaceIdiom == .pad ? 50 : 30)
+        }
     }
     
     
@@ -31,35 +110,74 @@ struct OnBoardingTutorialView: View {
     }
     
     private let onBoardingStateTutorialOnIphone: [ OnboardingState: String ] = [
-        .flipObject: "Flip the object",
-    ]
+        .flipObject: "ScanPasses-iPhone-FixedHeight-2",
+        .flipObjectASecondTime: "ScanPasses-iPhone-FixedHeight-3",
+        .captureFromLowerAngle: "ScanPasses-iPhone-FixedHeight-unflippable-low",
+        .captureFromHigherAngle: "ScanPasses-iPhone-FixedHeight-unflippable-high"    ]
     
     private let onBoardingStateTutorialOnIpad: [ OnboardingState: String ] = [
-        .flipObject: "Flip the object",
-    ]
+        .flipObject: "ScanPasses-iPad-FixedHeight-2",
+        .flipObjectASecondTime: "ScanPasses-iPad-FixedHeight-3",
+        .captureFromLowerAngle: "ScanPasses-iPad-FixedHeight-unflippable-low",
+        .captureFromHigherAngle: "ScanPasses-iPad-FixedHeight-unflippable-high"    ]
     
     private var tutorialURL: URL?{
-        return nil
+        let videoName : String
+        if UIDevice.current.userInterfaceIdiom == .pad{
+            videoName = onBoardingStateTutorialOnIpad[onBoardingFSM.currentState] ?? "ScanPasses-iPad-FixedHeight-1"
+        }else{
+            videoName = onBoardingStateTutorialOnIphone[onBoardingFSM.currentState] ?? "ScanPasses-iPhone-FixedHeight-1"
+            
+        }
+        
+        return Bundle.main.url(forResource: videoName, withExtension: "mp4")
+        
     }
     
-    private func getOrbitImageName(orbit: AppDataModel.Orbit) -> String {
-        return ""
+    private func getOrbitImageName(orbit: AppDataModel.Orbit) -> String? {
+        guard let session = appModel.objectCaptureSession else { return nil }
+        let orbitCompleted = session.userCompletedScanPass
+        let orbitCompleteImage = orbit <= appModel.orbit ? orbit.imageSelected : orbit.image
+        let orbitInCompleteImage = orbit < appModel.orbit ? orbit.imageSelected : orbit.image
+        return orbitCompleted ? orbitCompleteImage : orbitInCompleteImage
     }
     
-    private let onboardingStateToTitleMap: [ OnboardingState: String ] = [
+    private let onboardingStateToTitle: [ OnboardingState: String ] = [
         .tooFewImages: LocalizedString.tooFewImagesTitle,
-
-        ]
+        .firstSegmentNeedsWork: LocalizedString.firstSegmentNeedsWorkTitle,
+        .firstSegmentComplete: LocalizedString.firstSegmentCompleteTitle,
+        .secondSegmentNeedsWork: LocalizedString.secondSegmentNeedsWorkTitle,
+        .secondSegmentComplete: LocalizedString.secondSegmentCompleteTitle,
+        .thirdSegmentNeedsWork: LocalizedString.thirdSegmentNeedsWorkTitle,
+        .thirdSegmentComplete: LocalizedString.thirdSegmentCompleteTitle,
+        .flipObject: LocalizedString.flipObjectTitle,
+        .flipObjectASecondTime: LocalizedString.flipObjectASecondTimeTitle,
+        .flippingObjectNotRecommended: LocalizedString.flippingObjectNotRecommendedTitle,
+        .captureFromLowerAngle: LocalizedString.captureFromLowerAngleTitle,
+        .captureFromHigherAngle: LocalizedString.captureFromHigherAngleTitle,
+        .captureInAreaMode: LocalizedString.captureInAreaModeTitle
+    ]
     
     private var title: String {
-        return ""
+        onboardingStateToTitle[onBoardingFSM.currentState] ?? ""
     }
     
     private let onboardingStateToDetail: [ OnboardingState: String ] = [
         .tooFewImages: String(format: LocalizedString.tooFewImagesDetailText, AppDataModel.minimumNumberOfImages),
-        ]
-
+        .firstSegmentNeedsWork: LocalizedString.firstSegmentNeedsWorkDetailText,
+        .firstSegmentComplete: LocalizedString.firstSegmentCompleteDetailText,
+        .secondSegmentNeedsWork: LocalizedString.secondSegmentNeedsWorkDetailText,
+        .secondSegmentComplete: LocalizedString.secondSegmentCompleteDetailText,
+        .thirdSegmentNeedsWork: LocalizedString.thirdSegmentNeedsWorkDetailText,
+        .thirdSegmentComplete: LocalizedString.thirdSegmentCompleteDetailText,
+        .flipObject: LocalizedString.flipObjectDetailText,
+        .flipObjectASecondTime: LocalizedString.flipObjectASecondTimeDetailText,
+        .flippingObjectNotRecommended: LocalizedString.flippingObjectNotRecommendedDetailText,
+        .captureFromLowerAngle: LocalizedString.captureFromLowerAngleDetailText,
+        .captureFromHigherAngle: LocalizedString.captureFromHigherAngleDetailText,
+        .captureInAreaMode: LocalizedString.captureInAreaModeDetailText        ]
+    
     private var detail: String {
-        return ""
+        onboardingStateToDetail[onBoardingFSM.currentState] ?? ""
     }
 }
