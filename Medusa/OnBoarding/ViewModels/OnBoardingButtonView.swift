62276e7c4a79f7a87be972835cc8bb48f67e7e15
ZheerBarzan <zheerbarzan85@outlook.com>
2025-02-10 16:39:15 +0300

    finished with the onboarding buttons

--- Medusa/OnBoarding/ViewModels/OnBoardingButtonView.swift
+++ Medusa/OnBoarding/ViewModels/OnBoardingButtonView.swift
@@ -22,7 +22,79 @@ struct OnBoardingButtonView: View {
     
     
     var body: some View {
-        Text("OnBoarding Button View")
+        
+        VStack{
+            HStack{
+                CancelButton(ButtonLabel: "Cancel", onBoardingView: $onBoardingView)
+                    .padding(.leading)
+                Spacer()
+                CameraToggleButton(shotLocation: $shotLocation)
+                    .padding([.top , .trailing])
+                    .opacity(tutorialPlaying ? 0 : 1)
+            }
+            Spacer()
+            VStack(spacing: 0){
+                let currentStateInputs = onBoardingFSM.currentUserInput()
+                
+                if currentStateInputs.contains(where: { $0 == .continue(isFlippable: false) || $0 == .continue(isFlippable: true) }){
+                    CreateButton(buttonLabel: LocalizedString.continue,
+                                 buttonLabelColor: .white,
+                                 shouldApplyBackground: true,
+                                 action: { transition(with: .continue(isFlippable: appModel.isObjectFilppable)) }
+                    )
+                }
+                if currentStateInputs.contains(where: { $0 == .flipObjectAnyway }) {
+                    CreateButton(buttonLabel: LocalizedString.flipAnyway,
+                                 buttonLabelColor: .black,
+                                 action: {
+                        userHasIndicatedFlipObjectAnyway = true
+                        transition(with: .flipObjectAnyway)
+                    })
+                }
+                if currentStateInputs.contains(where: { $0 == .skip(isFlippable: false) || $0 == .skip(isFlippable: true) }) {
+                    CreateButton(buttonLabel: LocalizedString.skip,
+                                 buttonLabelColor: .black,
+                                 action: {
+                        transition(with: .skip(isFlippable: appModel.isObjectFilppable))
+                    })
+                }
+                if currentStateInputs.contains(where: { $0 == .finish }) {
+                    let buttonLabel = appModel.captureMode == .scene ? LocalizedString.process : LocalizedString.finish
+                    let buttonLabelColor: Color = appModel.captureMode == .scene ? .white :
+                    (onBoardingFSM.currentState == .thirdSegmentComplete ? .white : .black)
+                    let shouldApplyBackground = appModel.captureMode == .scene ? true : (onBoardingFSM.currentState == .thirdSegmentComplete)
+                    let showBusyIndicator = session.state == .finishing && !appModel.isDraftEnabled ? true : false
+                    CreateButton(buttonLabel: buttonLabel,
+                                 buttonLabelColor: buttonLabelColor,
+                                 shouldApplyBackground: shouldApplyBackground,
+                                 busyIndicator: showBusyIndicator,
+                                 action: { [weak session] in session?.finish() })
+                }
+                if currentStateInputs.contains(where: { $0 == .objectCannotBeFlipped }) {
+                    CreateButton(buttonLabel: LocalizedString.cannotFlipYourObject,
+                                 buttonLabelColor: .black,
+                                 action: {
+                        userHasIndicatedObjectCannotBeFlipped = true
+                        transition(with: .objectCannotBeFlipped)
+                    })
+                }
+                if onBoardingFSM.currentState == OnboardingState.tooFewImages ||
+                    onBoardingFSM.currentState == .secondSegmentComplete  ||
+                    onBoardingFSM.currentState == .thirdSegmentComplete {
+                    CreateButton(buttonLabel: "", action: {})
+                }
+                if currentStateInputs.contains(where: { $0 == .saveDraft }) {
+                    let showBusyIndicator = session.state == .finishing && appModel.isDraftEnabled ? true : false
+                    CreateButton(buttonLabel: LocalizedString.saveDraft,
+                                 buttonLabelColor: .black,
+                                 busyIndicator: showBusyIndicator,
+                                 action: { [weak appModel] in
+                        appModel?.saveDraft()
+                    })
+                }
+            }            .padding(.bottom)
+            
+        }
     }
     
     
@@ -36,36 +108,140 @@ struct OnBoardingButtonView: View {
     }
     
     private func reloadData(){
+        switch onBoardingFSM.currentState {
+        case .firstSegment, .dismiss:
+            onBoardingView = false
+        case .secondSegment, .thirdSegment, .additionalOrbitOnCurrentSegment:
+            newOrbit()
+        default:
+            break
+        }
         
     }
     
     private func newOrbit(){
+        if let userHasIndicatedObjectCannotBeFlipped{
+            appModel.objectCannotBeFlipped = userHasIndicatedObjectCannotBeFlipped
+        }
         
+        if let userHasIndicatedFlipObjectAnyway{
+            appModel.objectCanBeFlippedAnyway = userHasIndicatedFlipObjectAnyway
+            
+        }
+        
+        if !appModel.isObjectFilppable && !appModel.objectCanBeFlippedAnyway{
+            session.beginNewScanPass()
+        }else{
+            session.beginNewScanPassAfterFlip()
+            appModel.isObjectFlipped = true
+        }
+        
+        onBoardingView = false
+        appModel.orbit = appModel.orbit.next()
     }
     
     private func transition(with input: OnboardingUserInput){
+        do{
+            try onBoardingFSM.enter(input)
+            reloadData()
+        }catch{
+            print("could not transition to the requested state")
+        }
         
     }
 }
 
 
 private struct CreateButton: View {
+    @Environment(AppDataModel.self) var appModel
+    let buttonLabel: String
+    var buttonLabelColor: Color = .white
+    var buttonLabelBackgroundColor: Color = .black
+    var shouldApplyBackground: Bool = false
+    var busyIndicator: Bool = false
+    var action: () -> Void
+    
+    @Environment(\.colorScheme) private var colorScheme
+    
     var body: some View {
-        Text("Create Button")
+        Button(action: {
+            action()
+            
+        },label: {
+            ZStack{
+                if busyIndicator{
+                    HStack{
+                        Text(buttonLabel).hidden()
+                        Spacer().frame(maxWidth: 48)
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: shouldApplyBackground ? .white : (colorScheme == .light ? .black : .white)))
+                    }
+                }
+                Text(buttonLabel)
+                    .font(.headline)
+                    .bold()
+                    .foregroundColor(buttonLabelColor)
+                    .padding(16)
+                    .frame(maxWidth: shouldApplyBackground ? .infinity : nil)
+            }
+            
+        })
+        
+        .frame(maxWidth: .infinity)
+        .background{
+            if shouldApplyBackground{
+                RoundedRectangle(cornerRadius: 16).fill( buttonLabelBackgroundColor)
+                
+            }
+        }
+        .padding(.horizontal)
+        .frame(maxWidth: UIDevice.current.userInterfaceIdiom == .pad ? 380 : .infinity)
+        
     }
 }
 
 
 private struct CancelButton : View {
+    @Environment(AppDataModel.self) var appModel
+    let ButtonLabel: String
+    @Binding var onBoardingView: Bool
     var body: some View {
-        Text("Cancel Button")
+        Button(action:{
+            onBoardingView = false
+        },label: {
+            Text(ButtonLabel)
+                .font(.headline)
+                .bold()
+                .foregroundColor(.red)
+            
+        })
     }
 }
 
 
 private struct CameraToggleButton: View {
+    @Binding var shotLocation: Bool
     
     var body: some View {
-        Text("Camera Toggle Button")
+        VStack(spacing: 0){
+            Button(action:{
+                shotLocation.toggle()
+            }, label: {
+                Image(systemName: "camera.viewfinder")
+                    .font(.title)
+                    .foregroundColor(.black)
+                
+            })
+            .padding(.all, 5)
+            .background(.ultraThinMaterial.opacity(shotLocation ? 1 : 0))
+            .cornerRadius(15)
+            
+            
+            Text("Show capture positions")
+                .font(.caption2)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .frame(width: 80)
+        }
     }
 }
